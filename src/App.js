ecf83bcf66df0ac19cc517dfda15fa8aa8ed579f
rebecca15sousa <64431799+rebecca15sousa@users.noreply.github.com>
2021-02-26 14:18:48 -0500

    Created PokemonList component and displayed it on screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [])
+
+  
+
   return (
-    null
+    <PokemonList pokemon={pokemon} />
   );
 }
 
