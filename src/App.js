fa3033a73ba9bdc0ce45d4ced9630e68fa26b892
rebecca15sousa <64431799+rebecca15sousa@users.noreply.github.com>
2021-02-26 14:43:00 -0500

    Created loading state

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ function App() {
   const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
   const [nextPageUrl, setNextPageUrl] = useState()
   const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
     axios.get(currentPageUrl).then(res => {
+      setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
   }, [currentPageUrl])
 
-  
+  if (loading) return "Loading..."
 
   return (
     <PokemonList pokemon={pokemon} />
