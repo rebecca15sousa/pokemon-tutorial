ddc3d4c8c2fcf23d4be6c99effcf223ec33794e2
rebecca15sousa <64431799+rebecca15sousa@users.noreply.github.com>
2021-02-26 14:36:47 -0500

    Created pagination states

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    axios.get(currentPageUrl).then(res => {
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
-  }, [])
+  }, [currentPageUrl])
 
   
 
